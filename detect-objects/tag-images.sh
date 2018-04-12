b2916dff7b23e2800fda1c921de8291e4533d4fd
Daniel Heinrich <dannynullzwo@gmail.com>
2018-04-12 18:32:53 +0200

    more escaping in bash script

--- detect-objects/tag-images.sh
+++ detect-objects/tag-images.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 BASEDIR=$(dirname "$0")
-python3 "$BASEDIR"/detect_objects.py "$@" | awk -F '::' '{system("exiftool -overwrite_original -iptc:keywords='"'"'" $2 "'"'"' " $1)}'
+python3 "$BASEDIR"/detect_objects.py "$@" | awk -F '::' '{system("exiftool -overwrite_original -iptc:keywords='"'"'" $2 "'"'"' \"" $1 "\"")}'
