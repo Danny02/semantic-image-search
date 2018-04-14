8a9f5cf10f95f285585564847bddf016d8b8aff7
Daniel Heinrich <dannynullzwo@gmail.com>
2018-04-14 02:02:16 +0200

    better names for detection scripts

---
+++ detect-objects/tag-images-detect-objects.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+BASEDIR=$(dirname "$0")
+python3 "$BASEDIR"/detect_objects.py "$@" | awk -F '::' '{system("exiftool -overwrite_original -iptc:keywords+='"'"'" $2 "'"'"' \"" $1 "\"")}'
--- detect-objects/tag-images-with-objects.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-BASEDIR=$(dirname "$0")
-python3 "$BASEDIR"/detect_objects.py "$@" | awk -F '::' '{system("exiftool -overwrite_original -iptc:keywords='"'"'" $2 "'"'"' \"" $1 "\"")}'
